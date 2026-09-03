752fbcd8c421bfeacc59c279fbd2ccd9feefb1d1
Papa <alex94_ex@yahoo.com>
2026-09-03 12:52:45 +0300

    Add harvest configuration for 2026

---
+++ harvest-config.js
@@ -0,0 +1,7 @@
+window.harvestConfig = Object.freeze({
+  harvestYear: 2026,
+  mustStartDate: '2026-09-15T00:00:00+03:00',
+  mustEndDate: '2026-09-22T23:59:59+03:00',
+  nextHarvestYear: 2027,
+  totalBottles: 100
+});
