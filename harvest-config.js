2e50a88e459c4feb46887fb5566ad635cf279161
Papa <alex94_ex@yahoo.com>
2026-09-21 10:08:33 +0300

    Update harvest-config.js

--- harvest-config.js
+++ harvest-config.js
@@ -1,6 +1,6 @@
 window.harvestConfig = Object.freeze({
   harvestYear: 2026,
-  mustStartDate: '2026-09-21T00:00:00+03:00',
+  mustStartDate: '2026-09-20T00:00:00+03:00',
   mustEndDate: '2026-09-24T08:00:00+03:00',
   totalBottles: 100
 });
