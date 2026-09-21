761fec104133c67d0e33fa4236cbdf0ff55b4cfc
Papa <alex94_ex@yahoo.com>
2026-09-21 10:00:54 +0300

    Set harvest countdown end to September 24 at 08:00

--- harvest-config.js
+++ harvest-config.js
@@ -1,6 +1,6 @@
 window.harvestConfig = Object.freeze({
   harvestYear: 2026,
   mustStartDate: '2026-09-24T00:00:00+03:00',
-  mustEndDate: '2026-09-30T23:59:59+03:00',
+  mustEndDate: '2026-09-24T08:00:00+03:00',
   totalBottles: 100
 });
